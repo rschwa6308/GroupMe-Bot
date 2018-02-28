8a24dcdb5c73dca46e40d56d1f8c33fbabeeb941
Russell Schwartz <russell.w.schwartz@gmail.com>
2018-02-28 13:53:49 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,83 @@
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
+
+from flask import Flask, request
+
+import os
+import json
+from time import sleep
+import string
+import random
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['POST'])
+def webhook():
+    data = request.get_json()
+
+    # Ignore messages sent by self
+    if data['name'] != 'Mr. Roboto':
+        if data['text'][0] == "!":
+            process_command(data)
+
+    return "ok", 200
+
+
+def process_command(data):
+    command = data['text'][1:].split(' ')[0]
+    body = data['text'][1+len(command):]
+
+    if command == 'joke':
+        # send_message('Where did Tigger look for Pooh?')
+        # sleep(2)
+        # send_message('In the toilet!')
+        send_message(
+            '\"You should make !repeat allow arbitrary string echoing, and !exec allow arbitrary code execution. :-)\"')
+
+    elif command == 'randimg':
+        image_url = random_imgur_url()
+        send_message(image_url)
+
+
+def send_message(msg):
+    url = 'https://api.groupme.com/v3/bots/post'
+    data = {
+        'bot_id': os.getenv('GROUPME_BOT_ID'),
+        'text': msg,
+    }
+    request = Request(url, urlencode(data).encode())
+    json = urlopen(request).read().decode()
+
+
+def send_image(image):
+    image_url = upload_image(image)['url']
+    print(image_url)
+
+
+def upload_image(image):
+    url = 'https://image.groupme.com/pictures'
+    data = {
+        'file': image
+    }
+    request = Request(url, urlencode(data).encode())
+    json = urlopen(request).read().decode()
+    image_urls = json['payload']
+    print(image_urls)
+    return image_urls
+
+
+def random_imgur_url():
+    base_url = 'http://i.imgur.com/'
+    extension = '.jpg'
+    id_symbols = string.digits + string.ascii_letters
+
+    max_attempts = 100
+
+    for _ in range(max_attempts):
+        image_id = ''.join(random.choice(id_symbols) for _ in range(5))
+        image_url = base_url + image_id + extension
+        # Test if valid image
+        img = urlopen(image_url).read()
+        if len(img) != 503:     # length of 'removed.png'
+            return image_url
